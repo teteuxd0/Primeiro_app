1f1a841b6aad9c299eed9f33a7721d2f2238888a
teteuxd0 <m4theuzym@gmail.com>
2026-03-11 17:22:17 -0300

    Adicionando projeto

---
+++ Primeiro_app1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Primeiro_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
